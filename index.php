c75b2ecfb6e09647e7860969320fd4d5b37a0b9f
BamX <bamx23@gmail.com>
2010-12-06 15:23:36 +0200

    Исходная точка для сайта. Пустой шаблон :)

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+		<title>RPGEngine - Новая жизнь для форумных ролевых игр</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="core/jquery.js"></script>
+	</head>
+	<body>
+        <script type="text/javascript" src="core/core.js"></script>
+	</body>
+</html>
